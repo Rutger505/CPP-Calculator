a2568fd3dec0ec4d6ca6a1b742f584b398c20e6d
Rutger <rutger.pronk11@gmail.com>
2023-12-23 20:33:33 +0100

    initial plan

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Welcome to my calculator" << std::endl;
+    std::cout << "Please your first calculation. (type help for supported operations)" << std::endl;
+
+    // Get user input
+
+    // if user input is help, print help message and return to step 1
+
+    // remove whitespace from user input
+
+    // check if user input has any unsupported operations
+
+    // check if user input has unclosed parentheses
+
+    // check if user input has an operator at the end
+
+    // determine the order of operations
+
+    // calculate the result
+
+    // print result
+    return 0;
+}
