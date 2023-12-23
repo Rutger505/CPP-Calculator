aab01cf7301e4db5e666eeb5f190f0a26f11e1fb
Rutger <rutger.pronk11@gmail.com>
2023-12-23 23:01:06 +0100

    Make half priority system

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <ctgmath>
+#include <vector>
 
 
 enum class Operations {
     Add = '+',
     Subtract = '-',
     Multiply = '*',
-    Device = '/',
+    Devide = '/',
     MODULO = '%',
     POWER = '^'
 };
 
 bool isSupportedOperation(char c) {
     return static_cast<Operations>(c) == Operations::Add || static_cast<Operations>(c) == Operations::Subtract ||
-           static_cast<Operations>(c) == Operations::Multiply || static_cast<Operations>(c) == Operations::Device ||
+           static_cast<Operations>(c) == Operations::Multiply || static_cast<Operations>(c) == Operations::Devide ||
            static_cast<Operations>(c) == Operations::MODULO || static_cast<Operations>(c) == Operations::POWER;
 }
 
@@ -42,6 +43,17 @@ bool unSupportedCharactersInString(const std::string *string) {
     return false;
 }
 
+std::vector<Operations> operationsInCalculation(const std::string *string) {
+    std::vector<Operations> operations;
+    for (char c: *string) {
+        if (isSupportedOperation(c)) {
+            operations.push_back(Operations(c));
+
+        }
+    }
+    return operations;
+}
+
 
 /**
  * Calculate one operation.
@@ -56,7 +68,7 @@ double calculate(double firstNumber, Operations operation, double secondNumber) 
             return firstNumber - secondNumber;
         case Operations::Multiply:
             return firstNumber * secondNumber;
-        case Operations::Device:
+        case Operations::Devide:
             return firstNumber / secondNumber;
         case Operations::MODULO:
             return static_cast<int>(firstNumber) % static_cast<int>(secondNumber);
@@ -67,8 +79,50 @@ double calculate(double firstNumber, Operations operation, double secondNumber) 
     }
 }
 
+std::tuple<double, Operations, double>
+getOperationAroundOperator(const std::string *completeOperation, Operations operatorChar) {
+    std::vector<char> firstNumberChars;
+    std::vector<char> secondNumberChars;
+
+    int indexOperator = completeOperation->find(static_cast<char>(operatorChar));
+
+    // firstNumberString
+    int currentIndex = indexOperator;
+    do {
+        currentIndex--;
+        if (currentIndex < 0) {
+            break;
+        }
+        char currentChar = completeOperation->at(currentIndex);
+        if (isSupportedOperation(currentChar)) {
+            break;
+        }
+        firstNumberChars.insert(firstNumberChars.begin(), currentChar);
+    } while (true);
+
+    // secondNumberString
+    currentIndex = indexOperator;
+    do {
+        currentIndex++;
+        if (currentIndex >= completeOperation->length()) {
+            break;
+        }
+        char currentChar = completeOperation->at(currentIndex);
+        if (isSupportedOperation(currentChar)) {
+            break;
+        }
+        secondNumberChars.push_back(currentChar);
+    } while (true);
+
+    double firstNumber = std::stod(std::string(firstNumberChars.begin(), firstNumberChars.end()));
+    double secondNumber = std::stod(std::string(secondNumberChars.begin(), secondNumberChars.end()));
+
+    return std::make_tuple(firstNumber, operatorChar, secondNumber);
+}
+
 int main() {
     std::cout << "Hello, Welcome to my calculator" << std::endl;
+
 
     while (true) {
         std::cout << "Please your calculation. (type help for supported operations)" << std::endl;
@@ -96,40 +150,55 @@ int main() {
 
         // check if modulo operator is used with a double
 
-        // determine the order of operations
+        // get all operations in the calculation
+        std::vector<Operations> operations = operationsInCalculation(&userInput);
 
-        // get the extract the values out of string
-        std::string firstNumberString;
-        std::string secondNumberString;
-        Operations operation;
-        bool operatorFound = false;
-        for (char c: userInput) {
-            if (isSupportedOperation(c)) {
-                operation = Operations(c);
-                operatorFound = true;
-            } else if (operatorFound) {
-                secondNumberString += c;
-            } else {
-                firstNumberString += c;
+        // determine the order of operations
+        // modulos and power first
+        // then multiply and divide
+        // then add and subtract
+        std::vector<Operations> operationsInOrder;
+        for (Operations operation: operations) {
+            if (operation == Operations::MODULO || operation == Operations::POWER) {
+                operationsInOrder.push_back(operation);
             }
         }
-        double firstNumber = std::stod(firstNumberString);
-        double secondNumber = std::stod(secondNumberString);
+        for (Operations operation: operations) {
+            if (operation == Operations::Multiply || operation == Operations::Devide) {
+                operationsInOrder.push_back(operation);
+            }
+        }
+        for (Operations operation: operations) {
+            if (operation == Operations::Add || operation == Operations::Subtract) {
+                operationsInOrder.push_back(operation);
+            }
+        }
+
+        for (Operations operater: operationsInOrder) {
+
+            // get the extract the values out of string
+            auto tuple = getOperationAroundOperator(&userInput, operater);
+            double firstNumber = std::get<0>(tuple);
+            Operations operation = std::get<1>(tuple);
+            double secondNumber = std::get<2>(tuple);
 
 
-        // check if modulo operator is used with a double
-        if (operation == Operations::MODULO && (firstNumber != static_cast<int>(firstNumber) ||
-                                                secondNumber != static_cast<int>(secondNumber))) {
-            std::cout << "Modulo operator can only be used with integers" << std::endl;
-            continue;
+            // check if modulo operator is used with a double
+            if (operation == Operations::MODULO && (firstNumber != static_cast<int>(firstNumber) ||
+                                                    secondNumber != static_cast<int>(secondNumber))) {
+                std::cout << "Modulo operator can only be used with integers" << std::endl;
+                continue;
+            }
+
+
+            // calculate the result
+            double result = calculate(firstNumber, operation, secondNumber);
+
+            // print result
+            std::cout << result << std::endl;
         }
 
 
-        // calculate the result
-        double result = calculate(firstNumber, operation, secondNumber);
-
-        // print result
-        std::cout << result << std::endl;
     }
     return 0;
 }
