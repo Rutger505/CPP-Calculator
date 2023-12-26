c32b6f4491afce42dcf87401ce3f7eafee439e99
Rutger <rutger.pronk11@gmail.com>
2023-12-26 17:56:08 +0100

    Basic user input with processing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,56 @@
-int main() {
-    
+#include <iostream>
+#include <algorithm>
 
+void printSupportedOperators();
+
+void printHelp();
+
+void preProcessInput(std::string &input);
+
+void removeAllSpaces(std::string &input);
+
+int main() {
+    std::cout << "Welcome to my calculator" << std::endl;
+    std::cout << "Enter your calculation (h for help, q to quit)" << std::endl;
+
+    while (true) {
+        // get user input
+        std::string input;
+        std::getline(std::cin, input);
+
+        // check if user wants to quit
+        if (input == "q") {
+            break;
+        }
+
+        // check if user wants help
+        if (input == "h" || input == "help") {
+            printHelp();
+            continue;
+        }
+
+        preProcessInput(input);
+
+        std::cout << "Preprocessed input: " << input << std::endl;
+
+    }
+
+    std::cout << "Bye see you next time." << std::endl;
     return 0;
+}
+
+void printSupportedOperators() {
+    std::cout << "Supported operations are: +, -, *, /, ^, %" << std::endl;
+}
+
+void printHelp() {
+    printSupportedOperators();
+}
+
+void preProcessInput(std::string &input) {
+    removeAllSpaces(input);
+}
+
+void removeAllSpaces(std::string &input) {
+    input.erase(std::remove_if(input.begin(), input.end(), ::isspace), input.end());
 }
